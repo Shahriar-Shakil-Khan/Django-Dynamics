d231445d5611b3f22bed94f3a46050bfa6934a34
shahriar-shakil01797 <shahriarshakil28@gmail.com>
2025-07-16 12:42:55 +0600

    shakil

--- student/views.py
+++ student/views.py
@@ -11,13 +11,14 @@ def home(request):
         phone=request.POST.get('phone')
         password=request.POST.get('password')
         checkbox=request.POST.get('checkbox')
+        photo=request.FILES.get('photo')
         
         if checkbox=='on':
             checkbox=True
         else:
             checkbox=False
                    
-        student=models.Student(name=name,email=email, phone= phone,password=password,checkbox=checkbox)
+        student=models.Student(name=name,email=email, phone= phone,password=password,checkbox=checkbox,photo=photo)
         student.save()
         return HttpResponse("object created Successfully")
         
