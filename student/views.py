7f8768f68cbc73f167c24e3095b59983b1294850
shahriar-shakil01797 <shahriarshakil28@gmail.com>
2025-07-16 12:18:16 +0600

    shakil

---
+++ student/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render ,HttpResponse
+from .import models
+
+# Create your views here.
+
+def home(request):
+    print(request.POST)
+    if request.method=='POST':
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        phone=request.POST.get('phone')
+        password=request.POST.get('password')
+        checkbox=request.POST.get('checkbox')
+        
+        if checkbox=='on':
+            checkbox=True
+        else:
+            checkbox=False
+                   
+        student=models.Student(name=name,email=email, phone= phone,password=password,checkbox=checkbox)
+        student.save()
+        return HttpResponse("object created Successfully")
+        
+            
+            
+            
+        
+    return render(request,'student/index.html')
